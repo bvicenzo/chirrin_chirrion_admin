eb7b627ab6ddafaf28a91b56cb5e8a9b31f71fd8
Bruno Vicenzo <greenmetal@gmail.com>
2015-08-21 15:24:45 -0300

    Put toggles controller actions in a concern

--- app/controllers/chirrin_chirrion_admin/toggles_controller.rb
+++ app/controllers/chirrin_chirrion_admin/toggles_controller.rb
@@ -2,38 +2,6 @@ require_dependency "chirrin_chirrion_admin/application_controller"
 
 module ChirrinChirrionAdmin
   class TogglesController < ApplicationController
-    def index
-      @toggles = ChirrinChirrion.list
-    end
-
-    def new
-    end
-
-    def create
-      ChirrinChirrion.add_toggle(params[:name], {description: params[:description], active: params[:active].present?})
-      flash[:success] = 'Toggle has been created with success'
-      redirect_to(action: :index)
-    rescue ChirrinChirrion::Errors::ToggleIsRequired
-      flash.now[:error] = 'Toggle name must be filled.'
-      render(action: :new)
-    end
-
-    def chirrin
-      ChirrinChirrion.chirrin!(params[:id])
-      flash[:success] = 'Toggle has been activated with success'
-      redirect_to(action: :index)
-    end
-
-    def chirrion
-      ChirrinChirrion.chirrion!(params[:id])
-      flash[:success] = 'Toggle has been inactivated with success'
-      redirect_to(action: :index)
-    end
-
-    def destroy
-      ChirrinChirrion.remove_toggle(params[:id])
-      flash[:success] = 'Toggle has been removed with success'
-      redirect_to(action: :index)
-    end
+    include ChirrinChirrionAdmin::Concerns::Controllers::Toggles
   end
 end
--- lib/chirrin_chirrion_admin/engine.rb
+++ lib/chirrin_chirrion_admin/engine.rb
@@ -5,6 +5,13 @@ module ChirrinChirrionAdmin
     require 'jquery-rails'
     isolate_namespace ChirrinChirrionAdmin
 
+    # Loading Concerns
+    config.to_prepare do
+      Dir.glob(Engine.root + 'lib/concerns/**/*.rb').each do |concern|
+        require_dependency(concern)
+      end
+    end
+
     config.generators do |g|
       g.test_framework :rspec, fixture: false
       g.fixture_replacement :factory_girl, dir: 'spec/factories'
