8f2d6d3c69753f8213d057f7396edfc9c5a24015
Artur <artur.prado@gmail.com>
2019-10-06 15:42:45 -0300

    Add support to bootstrap

--- app/helpers/chirrin_chirrion_admin/application_helper.rb
+++ app/helpers/chirrin_chirrion_admin/application_helper.rb
@@ -1,4 +1,18 @@
 module ChirrinChirrionAdmin
   module ApplicationHelper
+    def chirrin_button
+      content_tag(:i, '', class: 'fa fa-flash chirrin') +
+        content_tag(:span, ' Chirrin!')
+    end
+
+    def chirrion_button
+      content_tag(:i, '', class: 'fa fa-flash chirrion') +
+        content_tag(:span, ' Chirrion!')
+    end
+
+    def remove_toggle_button
+      content_tag(:i, '', class: 'fa fa-trash remove') +
+        content_tag(:span, ' Remove')
+    end
   end
 end
