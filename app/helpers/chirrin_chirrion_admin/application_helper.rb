dcbfd7c95d56920fe079fbaeed00366075d2a455
Bruno Vicenzo <bvicenzo@users.noreply.github.com>
2021-01-22 17:09:52 -0300

    Add Frozen comments (#22)
    Co-authored-by: Bruno Vicenzo <bruno@alumni.usp.br>

--- app/helpers/chirrin_chirrion_admin/application_helper.rb
+++ app/helpers/chirrin_chirrion_admin/application_helper.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module ChirrinChirrionAdmin
   module ApplicationHelper
     def chirrin_button
--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   mount ChirrinChirrionAdmin::Engine => "/chirrin-chirrion-admin"
 end
